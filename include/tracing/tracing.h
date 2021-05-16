64fcb93c22c05d7f392ed371f1f0ea71f6e3c392
Martin Schröder <mkschreder.uk@gmail.com>
2021-05-17 00:10:59 +0200

    subsys: tracing: fixup
    Signed-off-by: Martin Schröder <mkschreder.uk@gmail.com>

--- include/tracing/tracing.h
+++ include/tracing/tracing.h
@@ -51,6 +51,25 @@
  * @}
  */
 
+/**
+ * @brief Power tracing APIs
+ * @defgroup power_tracing_apis Power Tracing APIs
+ * @ingroup tracing_apis
+ * @{
+ */
+
+/**
+ * @brief Trace power state enter
+ * @param power_state info object
+ */
+#define sys_trace_pm_power_state_set(state)
+
+/**
+ * @brief Trace power state exit
+ * @param power_state info object
+ */
+#define sys_trace_pm_power_state_exit(state)
+
 
 /**
  * @brief Tracing APIs
@@ -1926,27 +1945,6 @@
 /**
  * @}
  */ /* end of timer_tracing_apis */
-
-/**
- * @brief Power tracing APIs
- * @defgroup power_tracing_apis Power Tracing APIs
- * @ingroup tracing_apis
- * @{
- */
-
-/**
- * @brief Trace power state enter
- * @param power_state info object
- */
-#define sys_port_pm_state_enter(state)
-
-/**
- * @brief Trace power state exit
- * @param power_state info object
- */
-#define sys_port_pm_state_exit(state)
-
-
 
 /**
  * @}
